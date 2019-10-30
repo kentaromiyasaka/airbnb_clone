c0ccf5882f32f672363349dd5c5f08d328c16baa
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-30 11:57:22 +0800

    [Layout]Sign-up (#4)
    * [Layout]Sign-up
    
    * edited the header

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_10_29_040458) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "fullname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
