66da89392760ea38066f9c96f4dd76e1527acc53
kentaromiyasaka <kentamiya7426@gmail.com>
2019-10-31 13:53:18 +0800

    [Layout] Add Toastr Notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    user = User.new(user_params)
+    user.save
+    flash[:success] = "Successfully saved!"
+  end
 end
