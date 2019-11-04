bdd2456fe296d451545837afcfd3ed01d871dbb3
kentaromiyasaka <kentamiya7426@gmail.com>
2019-11-04 09:28:41 +0800

    Added flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def create
-    user = User.new(user_params)
-    user.save
-    flash[:success] = "Successfully saved!"
-  end
 end
