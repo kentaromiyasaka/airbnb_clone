ab165a9351d6fc387e4e463970bf20c75eaab0e8
kentaromiyasaka <kentamiya7426@gmail.com>
2019-11-05 11:38:18 +0800

    [Function]Facebook authentication

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
   def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://www.gravatar.com/avatar/#{gravatar_id}?size=250"
+    if user.image
+      "#{user.image}?type=large"
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://www.gravatar.com/avatar/#{gravatar_id}?size=250"
+    end
   end
 end
