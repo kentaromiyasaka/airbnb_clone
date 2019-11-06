dce351de3dc3d61eac1509405c4ab48be13c42d6
kentaromiyasaka <kentamiya7426@gmail.com>
2019-11-06 10:01:43 +0800

    changed controller and models

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,5 @@
 class RoomsController < ApplicationController
   def new
-    @room = Room.new
-  end
-
-  def rooms_params
-    params.require(:room).permit(:name,:price ,:address ,:home_type ,:room_type ,:guest_count ,:bedroom_count ,:bathroom_count ,:summary ,:has_tv ,:has_aircon ,:has_heating,:has_internet, :has_kitchen, :is_active)
+    @room = current_user.rooms.new
   end
 end
