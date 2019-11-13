ee70b0f3e7891d68b57096cae9ab70eade99a4d1
kentaromiyasaka <kentamiya7426@gmail.com>
2019-11-13 10:48:27 +0800

    [Function]Publish Room

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_paramaters, if: :devise_controller?
   include UsersHelper
+  include RoomsHelper
+
   protected
   def configure_permitted_paramaters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
